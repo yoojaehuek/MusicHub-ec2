e80d4d9ea204729ad38a4509c9ea5f2e1720364b
yoojh <jaehek2629@naver.com>
2024-01-30 13:16:23 +0900

    test

--- popol2/src/config/contansts.js
+++ popol2/src/config/contansts.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:8001";
-export const API_URL = "http://www.kimjunnyoung.com/app2";
+export const API_URL = "http://43.202.248.142/app2";
 // export const API_URL = "https://lamp-shopping-server-7ingout.herokuapp.com";
